020328146db0273f4e2d939abd130bf90b1e287f
Snakultime <snakultime@hotmail.fr>
2018-10-10 12:16:46 +0200

    maj 1.0

--- kikoo.rb
+++ kikoo.rb
@@ -1,3 +1,23 @@
-require 'dotenv'
+require "dotenv"
+
 Dotenv.load
-puts ENV['TWITTER_API_SECRET']
+
+require "twitter"
+
+require "pry"
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["TWITTER_CLIENT_KEY"]
+  config.consumer_secret     = ENV["CONSUMER_KEY_SECRET"]
+  config.access_token        = ENV["ACCESS_KEY_TOKEN"]
+  config.access_token_secret = ENV["ACCES_KEY_TOKEN_SECRET"]
+end
+
+client = Twitter::Streaming::Client.new do |config|
+  config.consumer_key        = "YOUR_CONSUMER_KEY"
+  config.consumer_secret     = "YOUR_CONSUMER_SECRET"
+  config.access_token        = "YOUR_ACCESS_TOKEN"
+  config.access_token_secret = "YOUR_ACCESS_SECRET"
+end
+
+# ligne qui permet de tweeter
